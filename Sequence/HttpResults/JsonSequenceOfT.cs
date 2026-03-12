a0d5007998a47629d1acd91a038bc4c37129644a
juner <juner@juner.net>
2026-03-13 05:56:24 +0900

    とちゅ

--- Sequence/HttpResults/JsonSequenceOfT.cs
+++ Sequence/HttpResults/JsonSequenceOfT.cs
@@ -1,56 +1,47 @@
-﻿using Juner.AspNetCore.Sequence.Internals;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Http.Metadata;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System.Reflection;
-using System.Text;
-using Microsoft.Extensions.Logging.Abstractions;
 #if NET8_0_OR_GREATER
 using System.Net.Mime;
-#else
-using System.Text.Json.Serialization.Metadata;
 #endif
 
 namespace Juner.AspNetCore.Sequence.HttpResults;
 
-public sealed class JsonSequence<T> : IResult, IEndpointMetadataProvider, IStatusCodeHttpResult, IValueHttpResult, IValueHttpResult<IAsyncEnumerable<T>>
+public sealed class JsonSequence<T> : Sequence<T>, IEndpointMetadataProvider
 {
-    readonly IEnumerable<T> _values = null!;
-    readonly IAsyncEnumerable<T> _asyncValues = null!;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="values"></param>
+    internal JsonSequence(IEnumerable<T> values) :base(values) { }
 
     /// <summary>
     /// 
     /// </summary>
     /// <param name="values"></param>
-    internal JsonSequence(IEnumerable<T> values) => _values = values;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="values"></param>
-    internal JsonSequence(IAsyncEnumerable<T> values) => _asyncValues = values;
+    internal JsonSequence(IAsyncEnumerable<T> values) : base(values) { }
 
     #region RS
     static ReadOnlyMemory<byte>? _rs;
     static ReadOnlyMemory<byte> RS => _rs ??= "\u001e"u8.ToArray();
     #endregion
 
+    protected override ReadOnlyMemory<byte> Begin => RS;
+
     #region LF
     static ReadOnlyMemory<byte>? _lf;
     static ReadOnlyMemory<byte> LF => _lf ??= "\n"u8.ToArray();
     #endregion
+    protected override ReadOnlyMemory<byte> End => LF;
 
     #region StatusCode
     const int STATUS_CODE = StatusCodes.Status200OK;
     /// <summary>
     /// Gets the HTTP status code: <see cref="StatusCodes.Status200OK"/>
     /// </summary>
-    public int StatusCode => STATUS_CODE;
-    int? IStatusCodeHttpResult.StatusCode => StatusCode;
+
+    public override int StatusCode => STATUS_CODE;
     #endregion
 
     #region ContentType
@@ -63,82 +54,8 @@ public sealed class JsonSequence<T> : IResult, IEndpointMetadataProvider, IStatu
     /// <summary>
     /// json-seq content type
     /// </summary>
-    public string ContentType => CONTENT_TYPE;
+    public override string ContentType => CONTENT_TYPE;
     #endregion
-
-    #region Value    
-    object? IValueHttpResult.Value => Value;
-
-    IAsyncEnumerable<T>? IValueHttpResult<IAsyncEnumerable<T>>.Value => Value;
-
-    /// <summary>
-    /// Gets the object result.
-    /// </summary>
-    public IAsyncEnumerable<T> Value => _get_value ??= GetValue();
-
-    IAsyncEnumerable<T>? _get_value = null;
-    IAsyncEnumerable<T> GetValue()
-    {
-        if (_asyncValues is not null)
-            return _asyncValues;
-        if (_values is not null)
-            return ToAsyncEnumerable(_values);
-        return ToAsyncEnumerable([]);
-        static async IAsyncEnumerable<T> ToAsyncEnumerable(IEnumerable<T> values)
-        {
-            await Task.Yield();
-            foreach (var value in values)
-                yield return value;
-        }
-    }
-    #endregion
-
-    /// <inheritdoc/>
-    public Task ExecuteAsync(HttpContext httpContext)
-    {
-        ArgumentNullException.ThrowIfNull(httpContext);
-
-        // Creating the logger with a string to preserve the category after the refactoring.
-        var logger = httpContext.RequestServices.GetService<ILogger<JsonSequence<T>>>() ?? NullLogger<JsonSequence<T>>.Instance;
-        
-        httpContext.Response.StatusCode = StatusCode;
-        if (string.IsNullOrEmpty(httpContext.Response.ContentType))
-            httpContext.Response.ContentType = ContentType;
-
-        var serializerOptions = (httpContext.RequestServices.GetService<IOptions<JsonOptions>>()?.Value ?? new JsonOptions()).SerializerOptions;
-#if !NET8_0_OR_GREATER
-        serializerOptions.TypeInfoResolver ??= new DefaultJsonTypeInfoResolver();
-#endif
-        var jsonTypeInfo = serializerOptions.GetTypeInfo<T>();
-        if (_asyncValues is not null)
-            return InternalFormatWriter
-                .WriteAsyncEnumerableAsync(
-                    values: _asyncValues,
-                    httpContext: httpContext,
-                    SerializerOptions: serializerOptions,
-                    JsonTypeInfo: jsonTypeInfo,
-                    Begin: RS,
-                    End: LF,
-                    SelectedEncoding: Encoding.UTF8,
-                    logger: logger,
-                    cancellationToken: httpContext.RequestAborted
-                );
-        if (_values is not null)
-            return InternalFormatWriter
-                .WriteEnumerableAsync(
-                    values: _values,
-                    httpContext: httpContext,
-                    SerializerOptions: serializerOptions,
-                    JsonTypeInfo: jsonTypeInfo,
-                    Begin: RS,
-                    End: LF,
-                    SelectedEncoding: Encoding.UTF8,
-                    logger: logger,
-                    cancellationToken: httpContext.RequestAborted
-
-                );
-        return Task.CompletedTask;
-    }
 
     /// <inheritdoc/>
     public static void PopulateMetadata(MethodInfo method, EndpointBuilder builder)
