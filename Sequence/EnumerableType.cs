b40edc1fb3a02bc660dc84309003549eac7830c0
juner <juner@juner.net>
2026-03-15 23:40:52 +0900

    add EnumerableType comment

--- Sequence/EnumerableType.cs
+++ Sequence/EnumerableType.cs
@@ -4,29 +4,29 @@
 namespace Juner.AspNetCore.Sequence;
 
 /// <summary>
-/// 
+/// enumerable type
 /// </summary>
 public enum EnumerableType
 {
     /// <summary>
-    /// 
+    /// use <see cref="IAsyncEnumerable{T}"/>
     /// </summary>
     AsyncEnumerable = 0,
 
     /// <summary>
-    /// 
+    /// use <see cref="IEnumerable{T}"/>
     /// </summary>
     Enumerable = 1,
     /// <summary>
-    /// 
+    /// use <see cref="System.Threading.Channels.ChannelReader{T}"/>
     /// </summary>
     ChannelReader = 2,
     /// <summary>
-    /// 
+    /// use <see cref="Array"/>
     /// </summary>
     Array = 3,
     /// <summary>
-    /// 
+    /// use <see cref="List{T}"/>
     /// </summary>
     List = 4,
 }
