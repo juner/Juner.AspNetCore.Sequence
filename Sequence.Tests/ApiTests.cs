a6812449049531b8e5a6b5f82564725fcc77bfe4
juner <juner@juner.net>
2026-03-17 11:02:50 +0900

    api テストの拡充と動作確認に伴う修正

--- Sequence.Tests/ApiTests.cs
+++ Sequence.Tests/ApiTests.cs
@@ -2,6 +2,7 @@ using Juner.AspNetCore.Sequence.Http;
 using Juner.AspNetCore.Sequence.Http.HttpResults;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,7 @@ public class ApiTests
     public required TestContext TestContext { get; init; }
     CancellationToken CancellationToken => TestContext.CancellationTokenSource.Token;
     [TestMethod]
-    public async Task NDJSON_Minimal_Post_Test()
+    public async Task NDJSON_Minimal_Post_1_Test()
     {
         using var server = await CreateServer();
         using var client = server.CreateClient();
@@ -35,6 +36,83 @@ public class ApiTests
         var text = await response.Content.ReadAsStringAsync(CancellationToken);
 
         Assert.Contains(body, text);
+        Assert.Contains(response.Headers.TryGetValues("Content-Type", out var v) ? v.First() : string.Empty, "application/x-ndjson");
+    }
+
+    [TestMethod]
+    public async Task NDJSON_Controller_Post_1_Test()
+    {
+        using var server = await CreateServer();
+        using var client = server.CreateClient();
+
+        var body =
+            "{\"name\":\"alice\",\"age\":30}\n" +
+            "{\"name\":\"bob\",\"age\":25}\n";
+
+        var message = new HttpRequestMessage(HttpMethod.Post, "/controller/person/ndjson");
+        var content = new StringContent(body, Encoding.UTF8, "application/x-ndjson");
+        message.Content = content;
+
+        message.Headers.Add("Accept", "application/x-ndjson");
+
+        var response = await client.SendAsync(message, CancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        var text = await response.Content.ReadAsStringAsync(CancellationToken);
+
+        Assert.Contains(body, text);
+        Assert.Contains(response.Headers.TryGetValues("Content-Type", out var v) ? v.First() : string.Empty, "application/x-ndjson");
+    }
+
+    [TestMethod]
+    public async Task NDJSON_Minimal_Post_2_negotiation_Test()
+    {
+        using var server = await CreateServer();
+        using var client = server.CreateClient();
+
+        var body =
+            "{\"name\":\"alice\",\"age\":30}\n" +
+            "{\"name\":\"bob\",\"age\":25}\n";
+
+        var message = new HttpRequestMessage(HttpMethod.Post, "/minimal/person/json");
+        var content = new StringContent(body, Encoding.UTF8, "application/x-ndjson");
+        message.Content = content;
+
+        message.Headers.Add("Accept", "application/x-ndjson");
+
+        var response = await client.SendAsync(message, CancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        var text = await response.Content.ReadAsStringAsync(CancellationToken);
+
+        Assert.Contains(body, text);
+        Assert.Contains(response.Headers.TryGetValues("Content-Type", out var v) ? v.First() : string.Empty, "application/x-ndjson");
+    }
+
+    [TestMethod]
+    public async Task NDJSON_Minimal_Post_3_Test()
+    {
+        using var server = await CreateServer();
+        using var client = server.CreateClient();
+
+        var body =
+            "{\"name\":\"alice\",\"age\":30}\n" +
+            "{\"name\":\"bob\",\"age\":25}\n";
+
+        var message = new HttpRequestMessage(HttpMethod.Post, "/minimal/person/only-ndjson");
+        var content = new StringContent(body, Encoding.UTF8, "application/x-ndjson");
+        message.Content = content;
+
+        var response = await client.SendAsync(message, CancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        var text = await response.Content.ReadAsStringAsync(CancellationToken);
+
+        Assert.Contains(body, text);
+        Assert.Contains(response.Headers.TryGetValues("Content-Type", out var v) ? v.First() : string.Empty, "application/x-ndjson");
     }
 
     [TestMethod]
@@ -70,6 +148,7 @@ public class ApiTests
         var text = await response.Content.ReadAsStringAsync(CancellationToken);
 
         Assert.Contains(body, text);
+        Assert.Contains(response.Headers.TryGetValues("Content-Type", out var v) ? v.First() : string.Empty, "application/json-seq");
     }
 
     [TestMethod]
@@ -91,6 +170,7 @@ public class ApiTests
         var text = await response.Content.ReadAsStringAsync(CancellationToken);
 
         Assert.Contains(body, text);
+        Assert.Contains(response.Headers.TryGetValues("Content-Type", out var v) ? v.First() : string.Empty, "application/jsonl");
     }
 
 #if NET8_0_OR_GREATER
@@ -131,28 +211,35 @@ public class ApiTests
             person.MapPost("json-seq", (Sequence<Person> sequence) => SequenceResults.JsonSequence(sequence));
             person.MapPost("ndjson", (Sequence<Person> sequence) => SequenceResults.NdJson(sequence));
             person.MapPost("jsonline", (Sequence<Person> sequence) => SequenceResults.JsonLine(sequence));
-            person.MapPost("json", (Sequence<Person> sequence) => sequence);
+            person.MapPost("json", (Sequence<Person> sequence) => SequenceResults.Sequence(sequence));
+            person.MapPost("only-json-seq", (Sequence<Person> sequence) => SequenceResults.Sequence(sequence, "application/jsonl"))
+                .Accepts<Sequence<Person>>("application/json-seq");
+            person.MapPost("only-jsonl", (Sequence<Person> sequence) => SequenceResults.Sequence(sequence, "application/jsonl"))
+                .Accepts<Sequence<Person>>("application/jsonl");
+            person.MapPost("only-ndjson", (Sequence<Person> sequence) => SequenceResults.Sequence(sequence, "application/x-ndjson"))
+                .Accepts<Sequence<Person>>("application/x-ndjson");
         });
     }
 }
-[ApiController]
+
+#pragma warning disable CA1822 // メンバーを static に設定します[ApiController]
 [Route("/controller/")]
 public class PersonController : ControllerBase
 {
-    [HttpPost("person/json-seq"), Consumes("application/json-seq")]
-    public ActionResult<IAsyncEnumerable<Person>> JsonSequence(IAsyncEnumerable<Person> person) => Ok(person);
+    [HttpPost("person/json-seq"), Consumes("application/json-seq"), Produces("application/json-seq")]
+    public ActionResult<IAsyncEnumerable<Person>> JsonSequence([FromBody]IAsyncEnumerable<Person> person) => Ok(person);
 
-    [HttpPost("person/ndjson"), Consumes("application/x-ndjson")]
-    public ActionResult<IAsyncEnumerable<Person>> NdJson(IAsyncEnumerable<Person> person) => Ok(person);
-    [HttpPost("person/jsonline"), Consumes("application/jsonl")]
-    public ActionResult<IAsyncEnumerable<Person>> JsonLine(IAsyncEnumerable<Person> person) => Ok(person);
+    [HttpPost("person/ndjson"), Consumes("application/x-ndjson"), Produces("application/x-ndjson")]
+    public ActionResult<IAsyncEnumerable<Person>> NdJson([FromBody]IAsyncEnumerable<Person> person) => Ok(person);
+    [HttpPost("person/jsonline"), Consumes("application/jsonl"), Produces("application/jsonl")]
+    public ActionResult<IAsyncEnumerable<Person>> JsonLine([FromBody]IAsyncEnumerable<Person> person) => Ok(person);
     [HttpPost("person-result/json-seq"), Consumes("application/json-seq")]
-    public JsonSequenceResult<Person> ResJsonSequence(IAsyncEnumerable<Person> person) => SequenceResults.JsonSequence(person);
+    public JsonSequenceResult<Person> ResJsonSequence([FromBody] IAsyncEnumerable<Person> person) => SequenceResults.JsonSequence(person);
 
     [HttpPost("person-result/ndjson"), Consumes("application/x-ndjson")]
-    public NdJsonResult<Person> ResNdJson(IAsyncEnumerable<Person> person) => SequenceResults.NdJson(person);
+    public NdJsonResult<Person> ResNdJson([FromBody] IAsyncEnumerable<Person> person) => SequenceResults.NdJson(person);
+
     [HttpPost("person-result/jsonline"), Consumes("application/jsonl")]
-    public JsonLineResult<Person> ResJsonLine(IAsyncEnumerable<Person> person) => SequenceResults.JsonLine(person);
-
-
+    public JsonLineResult<Person> ResJsonLine([FromBody] IAsyncEnumerable<Person> person) => SequenceResults.JsonLine(person);
 }
+#pragma warning restore CA1822 // メンバーを static に設定します
